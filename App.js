66ffc37bb0a40ad0f7234dfe5e058c9217913d51
Miguel Simijaca <miguelangelsimijaca@gmail.com>
2022-01-31 00:40:39 -0500

    change of image and adding description

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>"Hola, Coder!"</Text>
+      <Text>Curso React Native con expo</Text>
       <StatusBar style="auto" />
     </View>
   );
